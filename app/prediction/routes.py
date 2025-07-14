88ea751f94682d6cb0a95fff1fa092e8e09d57a7
Aaron Lim <aaron.lim.cstr@gmail.com>
2025-07-14 17:44:07 +0800

    feat: Add route for saving custom dataset entry

--- app/prediction/routes.py
+++ app/prediction/routes.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Response, status
+from numpy.typing import ArrayLike, NDArray
 import numpy as np
 
+import json
+from pathlib import Path
+from typing import Dict, List
 
 from app.prediction.architecture import model
-from app.prediction.schemas import PoseSequence
+from app.prediction.schemas import PoseSequence, DatasetBase
 
 router = APIRouter(prefix="/predict", tags=["lstm"])
 
@@ -16,17 +20,42 @@ OPTIMAL_THRESHOLDS_DICT = {
 
 
 @router.post("/api/predict/")
-def index(sequence: PoseSequence):
+def get_prediction(sequence: PoseSequence) -> Dict[str, List[float]]:
 
     # If no exercise name is provided, uses threshold for hiding face as default
-    threshold = OPTIMAL_THRESHOLDS_DICT.get(
+    threshold: ArrayLike = OPTIMAL_THRESHOLDS_DICT.get(
         sequence.exercise_name, OPTIMAL_THRESHOLDS_DICT["hiding_face"]
     )
-    np_landmarks = np.array(sequence.list_landmarks, dtype="float32").reshape(1, 20, 42)
+    np_landmarks: ArrayLike = np.array(
+        sequence.list_landmarks, dtype="float32"
+    ).reshape(1, 20, 42)
 
     np.set_printoptions(precision=3, suppress=True)
 
-    raw_pred = model.predict(np_landmarks)
-    binary_pred = (raw_pred >= threshold).astype(int)
+    raw_pred: NDArray = model.predict(np_landmarks)  # pyright: ignore[]
+    binary_pred: NDArray = (raw_pred >= threshold).astype(int)
 
     return {"prediction": binary_pred.tolist()}
+
+
+@router.post("/api/create-dataset")
+def create_dataset_entry(payload: DatasetBase):
+    correctness = "incorrect" if payload.is_incorrect else "correct"
+
+    dataset_dir: Path = Path(__file__).resolve().parent.parent
+    save_path: Path = dataset_dir / payload.exercise / correctness / payload.filename
+
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+
+    try:
+        with open(save_path, "w") as f:
+            json.dump(payload.model_dump, f, indent=2)
+
+    except Exception as error:
+        print("Error in saving payload to file: ", error)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error in saving payload to file: {error}",
+        )
+
+    return Response(status_code=status.HTTP_201_CREATED)
--- app/prediction/schemas.py
+++ app/prediction/schemas.py
@@ -1,7 +1,14 @@
-from typing import List
+from typing import List, Dict
 from pydantic import BaseModel
 
 
 class PoseSequence(BaseModel):
     list_landmarks: List[List[float]]
     exercise_name: str
+
+
+class DatasetBase(BaseModel):
+    filename: str
+    exercise: str
+    positions: Dict[str, Dict[str, List[float]]]
+    is_incorrect: bool
